88a84b436cb5fd2456d01233974359ba9c3d1188
an-tonic <aserdyukov@uclan.ac.uk>
2022-11-29 13:01:59 +0200

    Vector dist function. Pause, exit functionality. End game on off-island.

--- Shperes.io.cpp
+++ Shperes.io.cpp
@@ -1,7 +1,36 @@
 // Shperes.io.cpp: A program using the TL-Engine
 
 #include <TL-Engine.h>	// TL-Engine include file and namespace
+#include <iostream>
+
 using namespace tle;
+using namespace std;
+
+//Constants
+float kSphereSpeed = 0.05f;
+float kRotationSpeed = 0.05f;
+float kCamereSpeed = 0.05f;
+
+enum EGameState {Over, Running, Paused};
+EGameState gameState = Running;
+
+
+//Functions
+
+/// <summary>
+/// Calculates the vector length.
+/// </summary>
+/// <param name="a">The pointer to the "from" object.</param>
+/// <param name="b">The pointer to the "to" object.</param>
+/// <returns></returns>
+float vectorLen(IModel* a, IModel* b) {
+	float vectorX = b->GetX() - a->GetX();
+	float vectorY = b->GetY() - a->GetY();
+	float vectorZ = b->GetZ() - a->GetZ();
+
+	return sqrt(vectorX * vectorX + vectorY * vectorY + vectorZ * vectorZ);
+}
+
 
 void main()
 {
@@ -13,10 +42,10 @@ void main()
 	myEngine->AddMediaFolder( "C:\\Users\\aserdyukov\\source\\repos\\Games-Concepts-Spheres\\Assessment1Resources");
 
 	/**** Set up your scene here ****/
-	ICamera* myCamera = myEngine->CreateCamera(kFPS);
+	ICamera* myCamera = myEngine->CreateCamera(kManual);
 	//TODO: Change camera to Manual
-	//myCamera->RotateLocalX(90);
-	//myCamera->SetPosition(0, 200, 0);
+	myCamera->RotateLocalX(90);
+	myCamera->SetPosition(0, 200, 0);
 
 	IMesh* waterMesh = myEngine->LoadMesh("water.x");
 	IModel* water = waterMesh->CreateModel(0, -5, 0);
@@ -37,14 +66,79 @@ void main()
 	IModel* cube3 = cubeMesh->CreateModel(80, 5, -80);
 	IModel* cube4 = cubeMesh->CreateModel(-80, 5, -80);
 
+	IModel* cubes[] = {cube1, cube2, cube3 , cube4};
+
+
 	// The main game loop, repeat until engine is stopped
-	while (myEngine->IsRunning())
+	while (myEngine->IsRunning() && gameState != Over)
 	{
 		// Draw the scene
+		
 		myEngine->DrawScene();
+		
+		
 
 		/**** Update your scene each frame here ****/
 
+		if (gameState != Paused) {
+			//The sphere movement
+			if (myEngine->KeyHeld(Key_W)) {
+				sphere->MoveLocalZ(kSphereSpeed);
+			}
+			if (myEngine->KeyHeld(Key_S)) {
+				sphere->MoveLocalZ(-kSphereSpeed);
+			}
+			if (myEngine->KeyHeld(Key_A)) {
+				sphere->RotateLocalY(-kRotationSpeed);
+			}
+			if (myEngine->KeyHeld(Key_D)) {
+				sphere->RotateLocalY(kRotationSpeed);
+			}
+			//Camere movement
+			if (myEngine->KeyHeld(Key_Right)) {
+				myCamera->MoveX(kCamereSpeed);
+			}
+			if (myEngine->KeyHeld(Key_Left)) {
+				myCamera->MoveX(-kCamereSpeed);
+			}
+			if (myEngine->KeyHeld(Key_Up)) {
+				myCamera->MoveZ(kCamereSpeed);
+			}
+			if (myEngine->KeyHeld(Key_Down)) {
+				myCamera->MoveZ(-kCamereSpeed);
+			}
+
+		}
+		//Exit game
+		if (myEngine->KeyHit(Key_Escape)) {
+			gameState = Over;
+		}
+
+		//Game pause
+		if (myEngine->KeyHit(Key_P)) {
+			if (gameState == Running) {
+				gameState = Paused;
+				
+			}else{
+				gameState = Running;
+			}
+		}
+	//TODO: remove model properly.
+		//Cube proximity evaluator
+		for (IModel* cube : cubes){
+			if (vectorLen(sphere, cube) < 5 + 5) {
+				cubeMesh->RemoveModel(cube);
+				break;
+			}
+		}
+
+		
+
+		//Game over when outside of island
+		if (abs(sphere->GetX()) > 100 || abs(sphere->GetZ()) > 100) {
+			gameState = Over;
+		}
+		
 	}
 
 	// Delete the 3D engine now we are finished with it
